0dfccf199269c397d18bc8ccb6bdc17ae74fa4cc
Quasimodo <quasimodo1660@gmail.com>
2018-01-12 15:14:22 -0800

    Add random age to the input

--- Table View Demo/ViewController.swift
+++ Table View Demo/ViewController.swift
@@ -12,13 +12,16 @@ class ViewController: UIViewController {
     @IBOutlet var inputField: UITextField!
     @IBOutlet var tableP: UITableView!
     var tasks: [String]=[]
+    var ages:[Int]=[]
     @IBAction func buttonPressed(_ sender: Any) {
         tasks.append(inputField.text!)
+        ages.append(Int(arc4random_uniform(UInt32(95-5+1))))
         tableP.reloadData()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         tableP.dataSource = self
+        tableP.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -29,7 +32,7 @@ class ViewController: UIViewController {
 
 
 }
-extension ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
         // return an integer that indicates how many rows (cells) to draw
@@ -38,7 +41,16 @@ extension ViewController: UITableViewDataSource {
         // Get the UITableViewCell and create/populate it with data then return it
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
         cell.textLabel?.text = tasks[indexPath.row]
+        cell.detailTextLabel?.text = "\(String(ages[indexPath.row])) years old"
         return cell
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section: \(indexPath.section) and Row: \(indexPath.row)")
+        tasks.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
+
 }
+
+
 
