187edfa4dff79dbb2c1dfce231d23cfdfd136962
Quasimodo <quasimodo1660@gmail.com>
2018-01-12 15:25:09 -0800

    Add random background color to the cell

--- Table View Demo/ViewController.swift
+++ Table View Demo/ViewController.swift
@@ -13,9 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet var tableP: UITableView!
     var tasks: [String]=[]
     var ages:[Int]=[]
+    var colors:[UIColor]=[]
     @IBAction func buttonPressed(_ sender: Any) {
         tasks.append(inputField.text!)
         ages.append(Int(arc4random_uniform(UInt32(95-5+1))))
+        colors.append(generateRandomColor())
         tableP.reloadData()
     }
     override func viewDidLoad() {
@@ -29,7 +31,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func generateRandomColor() -> UIColor {
+        let hue : CGFloat = CGFloat(arc4random() % 256) / 256 // use 256 to get full range from 0.0 to 1.0
+        let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from white
+        let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from black
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+    }
 
 }
 extension ViewController: UITableViewDataSource, UITableViewDelegate{
@@ -42,6 +49,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate{
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
         cell.textLabel?.text = tasks[indexPath.row]
         cell.detailTextLabel?.text = "\(String(ages[indexPath.row])) years old"
+        cell.backgroundColor = colors[indexPath.row]
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
